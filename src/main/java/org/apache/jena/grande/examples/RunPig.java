9e57c4a294f297ef04077fa9d51ec5a4b5d02835
Paolo Castagna <paolo.castagna@gmail.com>
2012-04-11 11:10:35 +0100

    ...

--- src/main/java/org/apache/jena/grande/examples/RunPig.java
+++ src/main/java/org/apache/jena/grande/examples/RunPig.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 import org.apache.pig.PigServer;
 
 public class RunPig {
+	
+	private static final String output = "./target/output";
 
 	public static void main(String[] args) throws IOException {
 		PigServer pig = new PigServer("local");
+		pig.deleteFile(output);
 		// pig.debugOn();
-		pig.registerJar("./target/jena-grande-0.1-SNAPSHOT.jar");
+		// pig.registerJar("./target/jena-grande-0.1-SNAPSHOT.jar");
 		pig.registerQuery("quads = LOAD './src/test/resources/data.nq' USING org.apache.jena.grande.pig.RdfStorage() AS (g,s,p,o);");
 		pig.registerQuery("a = FILTER quads BY ( p == '<http://xmlns.com/foaf/0.1/name>' ) ;");
 		pig.registerQuery("b = FILTER a BY ( o == '\"Bob\"' ) ;");
-		pig.store("b", "./target/output", "org.apache.jena.grande.pig.RdfStorage()");
+		pig.store("b", output, "org.apache.jena.grande.pig.RdfStorage()");
 	}
 
 }
